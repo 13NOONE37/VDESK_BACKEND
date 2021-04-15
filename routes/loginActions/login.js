34f13dab39880fe6ad123dc03507b6c787340d5b
unknown <oliwerklauze22@gmail.com>
2021-04-15 22:24:55 +0200

    generowanie tokenu jwt

--- routes/loginActions/login.js
+++ routes/loginActions/login.js
@@ -2,6 +2,7 @@ const { json } = require('body-parser');
 const registerModel = require('../../database/LogModels/registerSchema');
 const bcrypt = require('bcrypt');
 const { set } = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res)=>{
 //hello wtorek
@@ -10,8 +11,13 @@ module.exports = async (req, res)=>{
 
         if(user) {
             const isValid = await bcrypt.compare(body.password, user.password);
+            const token = jwt.sign({id: user._id}, config.secret, {
+                expiresIn: 86400
+            });
 
-            if(isValid) return res.status(200).json({isValid:true})
+            if(isValid) return res.status(200).json({
+                isValid:true
+            })
             else return res.status(200).json({isValid:false})
         } else 
             res.status(200).json({isValid:false});
